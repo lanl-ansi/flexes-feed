3a60a5f1ed08a579843c79c24169621692827b74
James Arnold <arnold_j@lanl.gov>
2018-01-11 16:52:50 -0700

    Generalize Scraper to track multiple files

--- scraper.py
+++ scraper.py
@@ -8,9 +8,9 @@ from redis import StrictRedis
 REDIS_HOST = 'localhost'
 LAST_MODIFIED_CHANNEL = 'data-feeds:last-modified'
 
-class Scraper(object):
-    def __init__(self, url, s3_folder, channel, frequency=600):
-        self.url = url
+class Scraper:
+    def __init__(self, s3_folder, channel, frequency=600):
+        print('class init')
         self.dest = s3_folder
         self.channel = channel
         self.frequency = frequency
@@ -21,34 +21,36 @@ class Scraper(object):
         # Return list of new files
         raise NotImplementedError('check method must be overridden')
 
-    def publish(self, new_file, last_modified=datetime.utcnow()):
-        filename = os.path.basename(new_file)
-        s3_file = os.path.join(self.dest, filename)
-        with requests.get(new_file, stream=True) as response_stream:
-            s3_utils.stream_to_s3(response_stream, s3_file)
-        self.db.publish(self.channel, s3_file)
-        self.db.hset(LAST_MODIFIED_CHANNEL, self.channel, str(last_modified))
-
     def scrape(self):
         # Scrape page and return file update information
         raise NotImplementedError('scrape method must be overridden')
 
-    def last_modified(self):
-        t = self.db.hget(LAST_MODIFIED_CHANNEL, self.channel).decode()
-        return datetime.strptime(t, '%Y-%m-%d %H:%M:%S')
+    def publish(self, file_url, filename=None, subfolder='', last_modified=datetime.utcnow()):
+        filename = os.path.basename(file_url) if filename is None else filename
+        s3_file = os.path.join(self.dest, subfolder, filename)
+        with requests.get(new_file, stream=True) as response_stream:
+            s3_utils.stream_to_s3(response_stream, s3_file)
+        self.db.publish(self.channel, s3_file)
+        self.db.hset(LAST_MODIFIED_CHANNEL, new_file, str(last_modified))
+
+    def last_modified(self, filename):
+        # Create key in last-modified store 
+        # if it doesn't already exist set it 
+        # to 2000-01-01 00:00:00
+        if self.db.hexists(LAST_MODIFIED_CHANNEL, filename):
+            t = self.db.hget(LAST_MODIFIED_CHANNEL, filename).decode()
+            date_modified = datetime.strptime(t, '%Y-%m-%d %H:%M:%S')
+        else:
+            date_modified = datetime(2000, 1, 1)
+            self.db.hset(LAST_MODIFIED_CHANNEL, filename, str(date_modified))
+        return date_modified
 
     def run(self):
         try:
-            # Create key in last-modified store 
-            # if it doesn't already exist set it 
-            # to 2000-01-01 00:00:00
-            self.db.hsetnx(LAST_MODIFIED_CHANNEL, 
-                           self.channel, 
-                           str(datetime(2000, 1, 1)))
             while True:
                 new_files = self.check()
                 for f in new_files:
-                    publish(f)
+                    self.publish(f)
                 time.sleep(self.frequency)
         except KeyboardInterrupt:
             print('Stopping scraper')
