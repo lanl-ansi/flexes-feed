fd752ae1b1e6cfde2106177dd1733e96dff4d728
James Arnold <arnold_j@lanl.gov>
2018-01-11 16:53:23 -0700

    Update tests

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -6,36 +6,43 @@ import mock
 from datetime import datetime
 from scraper import Scraper
 
-def test_override():
-    return True
+class DummyScraper(Scraper):
+    def check(self):
+        return True
+
+    def scrape(self):
+        return True
 
 class TestScraper:
     def setup_method(self, _):
-        url = 'http://foo.com'
         s3_folder = 's3://bucket/path/to/folder'
         channel = 'test-data-feed'
-        self.scraper = Scraper(url, s3_folder, channel)
-        self.scraper.db = mock.MagicMock()
+        self.bad_scraper = Scraper(s3_folder, channel)
+        self.good_scraper = DummyScraper(s3_folder, channel)
+        self.good_scraper.db = mock.MagicMock()
 
     def test_check_not_implemented(self):
         with pytest.raises(NotImplementedError):
-            self.scraper.check()
+            self.bad_scraper.check()
 
     def test_check(self):
-        self.scraper.check = test_override
-        assert(self.scraper.check())
+        assert(self.good_scraper.check())
 
     def test_scrape_not_implemented(self):
         with pytest.raises(NotImplementedError):
-            self.scraper.scrape()
+            self.bad_scraper.scrape()
 
     def test_scrape(self):
-        self.scraper.scrape = test_override
-        assert(self.scraper.scrape())
+        assert(self.good_scraper.scrape())
     
     def test_last_modified(self):
-        d = datetime.now()
-        self.scraper.db.hget.return_value = b'2000-01-01 00:00:00'
-        last_modified = self.scraper.last_modified()
+        self.good_scraper.db.hexists.return_value = True
+        self.good_scraper.db.hget.return_value = b'2003-04-01 00:00:00'
+        last_modified = self.scraper.last_modified('http://foo.com/file.txt')
+        assert(last_modified == datetime(2003,4,1))
+
+    def test_last_modified(self):
+        self.good_scraper.db.hexists.return_value = False
+        last_modified = self.good_scraper.last_modified('http://foo.com/file.txt')
         assert(last_modified == datetime(2000,1,1))
 
