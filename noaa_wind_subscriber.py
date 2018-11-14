0466c43f5522cc89f4667ac9051175e5066b438c
James Arnold <arnold_j@lanl.gov>
2018-11-14 09:41:49 -0700

    Add publish step to the wind subscriber

--- noaa_wind_subscriber.py
+++ noaa_wind_subscriber.py
@@ -6,8 +6,11 @@ from data_feed.subscriber import Subscriber
 
 class WindForecastSubscriber(Subscriber):
     def process(self, s3_uri):
+        """Launch an API task to process the published wind forecast and publish the
+            result on a separate channel when the task completes.
+        """
         api_url = 'https://api.lanlytics.com'
-        service = 'ep-damage'
+        service = 'process-wind-forecast'
         data = {'service': service,
                 'command': {
                     'arguments': [
@@ -17,6 +20,10 @@ class WindForecastSubscriber(Subscriber):
                }
         try:
             result = run_task(api_url, data)
+
+            # Publish the processed wind forecast result 
+            # for processing by additional subscribers
+            self.db.publish(os.path.dirname(s3_uri), s3_uri)
         except ValueError as e:
             print(e)
 
