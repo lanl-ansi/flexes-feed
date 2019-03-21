46393a871b3e301af5186a78b827bf995d27d4a5
James Arnold <arnold_j@lanl.gov>
2019-03-21 13:33:38 -0600

    Fix configuration override

--- flexes_feed/config.py
+++ flexes_feed/config.py
@@ -2,7 +2,9 @@ import json
 import os
 from pathlib import Path
 
-def load_config(config_path='config.json'):
+PACKAGE_DIR = Path(__file__).resolve().parent
+
+def load_config(config_path=None):
     """Load the configuration for the data feed components. The configuration loader 
     will override values defined in the library's `default_config.json` file with 
     values defined in the `config_path` argument. By default this file is located in 
@@ -16,12 +18,11 @@ def load_config(config_path='config.json'):
     Returns:
         dict: A dictionary containing configuration variables
     """
-    default_path = os.path.join(os.path.dirname(__file__), 'default_config.json')
-    default = Path(default_path)
+    default = PACKAGE_DIR.joinpath('default_config.json')
     with default.open() as f:
         config = json.load(f)
 
-    user_config = Path(config_path)
+    user_config = Path(config_path) if config_path is not None else PACKAGE_DIR.joinpath('config.json')
     if user_config.exists():
         with user_config.open() as f:
             user = json.load(f)
